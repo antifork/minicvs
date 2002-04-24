295f89f4120a0a35dae4d4486f57e5f2a509317d
deneb <deneb@antifork.org>
2002-04-24 22:49:28 +0000

    Fixed a type  	root.c sanitize_env.c

--- client/sanitize_env.c
+++ client/sanitize_env.c
@@ -3,36 +3,38 @@
 #include "xmalloc.h"
 #include "sstrcpy.h"
 
-char *env_cvsroot(const char *mycvsroot)
+char *
+env_cvsroot (const char *mycvsroot)
 {
-char *sanitized;
-size_t env_sz = strlen(mycvsroot)+1;
-size_t pattern_sz = strlen(pattern);
-if (mycvsroot == NULL)
+  char *sanitized;
+  size_t env_sz = strlen (mycvsroot) + 1;
+  size_t pattern_sz = strlen (pattern);
+  if (mycvsroot == NULL)
     return NULL;
-if (strlen(mycvsroot) > MAX_ENV_VAR)
-        return NULL;
-    
-sanitized = xmalloc(env_sz);
-if (sstrcpy(sanitized,mycvsroot,env_sz)<0)
+  if (strlen (mycvsroot) > MAX_ENV_VAR)
+    return NULL;
+
+  sanitized = xmalloc (env_sz);
+  if (sstrcpy (sanitized, mycvsroot, env_sz) < 0)
     {
-    xfree(sanitized)
-    return NULL;
+      xfree (sanitized);
+      return NULL;
     }
 /* here the check */
-if (checks(sanitized,OK_CVSROOT)<0)
+  if (checks (sanitized, OK_CVSROOT) < 0)
     {
-    xfree(sanitized);
-    return NULL;
+      xfree (sanitized);
+      return NULL;
     }
-return sanitized;
+  return sanitized;
 }
 
-static int checks(char *to_check, const char *check)
-{ 
-char *cp;
-int bad = 0;
-for (cp = to_check;*(cp+=strspn(cp,check));)
-     bad = -1;
-return bad;
+static int
+checks (char *to_check, const char *check)
+{
+  char *cp;
+  int bad = 0;
+  for (cp = to_check; *(cp += strspn (cp, check));)
+    bad = -1;
+  return bad;
 }
